1342c2303e4d79df55d5f9dbb205eea7100d6ed5
Riajul-Islam-Dev <ritewu2014@gmail.com>
2024-02-13 10:31:53 +0600

    Formatted

--- resources/views/user/partial/header.blade.php
+++ resources/views/user/partial/header.blade.php
@@ -1,19 +1,17 @@
-
 <div class="page-main-header">
     <div class="main-header-right row m-0">
         <div class="main-header-left">
             <div class="logo-wrapper"><a href="{{ route('user.userdashboard') }}"><img class="img-fluid css1"
-                        src="{{ asset('assets/backend/images/logo/logo.PNG') }}" alt=""
-                        ></a></div>
+                        src="{{ asset('assets/backend/images/logo/logo.PNG') }}" alt=""></a></div>
             <div class="dark-logo-wrapper"><a href="{{ route('user.userdashboard') }}"><img class="img-fluid css2"
-                        src="{{ asset('assets/backend/images/logo/logo.PNG') }}" alt="" ></a></div>
+                        src="{{ asset('assets/backend/images/logo/logo.PNG') }}" alt=""></a></div>
             <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center"
                     id="sidebar-toggle"></i></div>
         </div>
         <div class="left-menu-header col">
             <ul>
                 <li>
-                    <h3 class="font-success css3" >Army Officers Club</h3>
+                    <h3 class="font-success css3">Army Officers Club</h3>
                 </li>
             </ul>
         </div>
